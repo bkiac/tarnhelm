0e7f9811b5f9af28f3cc289d44692bea40c9927d
Bence Knáb <benceknab.iac@gmail.com>
2020-04-17 12:20:43 +0200

    add restricted port config validator

---
+++ backend/src/config/formats.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+import convict from 'convict';
+import { isString } from 'lodash';
+
+import restrictedPorts from './restricted-ports';
+
+export default (): void => {
+  convict.addFormats({
+    string: {
+      validate: (value) => {
+        if (!isString(value) || value === '') {
+          throw new Error('must be a non-empty string');
+        }
+      },
+    },
+
+    /** The port can be used by the WebSocket API and is not restricted in a browser. */
+    'websocket-port': {
+      coerce: (value) => parseInt(value, 10),
+      validate: (value) => {
+        if (restrictedPorts.includes(value)) {
+          throw new Error('port is restricted in a browser');
+        }
+      },
+    },
+  });
+};
