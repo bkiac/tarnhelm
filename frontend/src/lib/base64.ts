54c52bd873a5ad58f9e365fa6e9666854bb2b395
Bence Knáb <benceknab.iac@gmail.com>
2020-05-04 20:49:03 +0200

    rename base64 toString function

--- frontend/src/lib/base64.ts
+++ frontend/src/lib/base64.ts
@@ -1,7 +1,8 @@
 import base64 from 'base64-js';
 
-export function fromArray(array: Uint8Array): string {
-  return base64.fromByteArray(array).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+export function fromArray(array: Uint8Array | ArrayBuffer): string {
+  const byteArray = array instanceof ArrayBuffer ? new Uint8Array(array) : array;
+  return base64.fromByteArray(byteArray).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
 }
 
 export function toArray(string: string): Uint8Array {
