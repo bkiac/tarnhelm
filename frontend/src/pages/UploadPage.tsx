35b83fa47ef245db9e0cacf528caf37f1b4fad6a
Bence Knáb <benceknab.iac@gmail.com>
2020-05-20 20:45:57 +0200

    change title

--- frontend/src/pages/UploadPage.tsx
+++ frontend/src/pages/UploadPage.tsx
@@ -10,23 +10,17 @@ const Container = styled.div`
 `;
 
 const H1 = styled.h1`
-  font-size: 2rem;
-  margin: 0;
-  margin-bottom: 0.5rem;
-`;
-
-const H2 = styled.h2`
+  text-align: center;
   font-size: 1.25rem;
   font-weight: normal;
   margin: 0;
-  margin-bottom: 1rem;
+  margin-bottom: 1.5rem;
 `;
 
 const UploadPage: React.FC = () => {
   return (
     <Container>
-      <H1>Tarnhelm Title</H1>
-      <H2>Tarnhelm Description</H2>
+      <H1>Share end-to-end encrypted files which expire automatically</H1>
       <Upload />
     </Container>
   );
