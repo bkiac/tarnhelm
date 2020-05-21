790f17b0b8f69a9fe7d3cb6514c76499338365ed
Bence Knáb <benceknab.iac@gmail.com>
2020-05-21 11:53:57 +0200

    add ping endpoint to wake up sleeping server

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,9 @@ import express from 'express';
 import storage from './storage';
 
 const router = express.Router();
+
+router.get('/ping', (req, res) => res.send());
+
 router.use('/', storage);
 
 export default router;
