9bea864ea86d7af3e72a2f734fc56911e4bd92bc
Ben Knáb <benknab@pm.me>
2021-02-21 22:51:11 +0100

    Remove tippin button

--- packages/web/src/components/Footer.tsx
+++ packages/web/src/components/Footer.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components"
 import { AppTitle } from "./AppTitle"
 import { ExternalLink } from "./ExternalLink"
 import { InternalLink } from "./InternalLink"
-import { TippinButton } from "./TippinButton"
 
 const Container = styled.div`
 	display: flex;
@@ -46,10 +45,6 @@ export const Footer: React.FunctionComponent = () => (
 			>
 				Node
 			</ExternalLink>
-		</Item>
-
-		<Item>
-			<TippinButton />
 		</Item>
 	</Container>
 )
