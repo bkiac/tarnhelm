1a3f8f48239f501f667a5442f1354d5c66214917
Ben Knáb <benknab@pm.me>
2021-02-21 22:33:51 +0100

    Fix missing tippin button from pre-rendered page

--- packages/web/src/components/TippinButton.tsx
+++ packages/web/src/components/TippinButton.tsx
@@ -1,11 +1,17 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
-export const TippinButton: React.FC = () => (
-	<>
-		<div id="tippin-button" data-dest="bkiac" />
-		<script
-			src="https://tippin.me/buttons/tip.js?0001"
-			type="text/javascript"
-		/>
-	</>
-)
+export const TippinButton: React.FC = () => {
+	const [, setInitialized] = useState(false)
+	useEffect(() => {
+		setInitialized(true)
+	}, [])
+	return (
+		<>
+			<div id="tippin-button" data-dest="bkiac" />
+			<script
+				src="https://tippin.me/buttons/tip.js?0001"
+				type="text/javascript"
+			/>
+		</>
+	)
+}
