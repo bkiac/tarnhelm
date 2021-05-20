b842ba169fe70fcdb19247344ee83fc33ad9fe9c
Ben Knáb <benknab@pm.me>
2021-05-20 11:16:02 +0200

    Fix pre-commit scripts

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-	"/packages/api/*.{ts,tsx}": () =>
-		"tsc --project /packages/api/tsconfig.json --noEmit",
-	"/packages/web/*.{ts,tsx}": () =>
-		"tsc --project /packages/web/tsconfig.json --noEmit",
+	"./packages/api/**/*.{ts,tsx}": () =>
+		"tsc --project ./packages/api/tsconfig.json --noEmit",
+	"./packages/web/**/*.{ts,tsx}": () =>
+		"tsc --project ./packages/web/tsconfig.json --noEmit",
 
-	"*.ts,tsx,js": "eslint --cache --fix",
+	"*.{ts,tsx,js}": "eslint --cache --fix",
 
 	"*.{ts,tsx,js,jsx,json,css,md}": "prettier --write",
 }
