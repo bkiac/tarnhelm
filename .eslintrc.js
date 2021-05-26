3f85bcd0f5c65fb190a900cae2b1a2139ca11087
Ben Knáb <benknab@pm.me>
2021-05-26 13:26:57 +0200

    fix: remove obsolete typescript config from root eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
 	root: true,
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		tsconfigRootDir: __dirname,
-		project: "./tsconfig.json",
-	},
 	ignorePatterns: [
 		"node_modules",
 		"dist",
