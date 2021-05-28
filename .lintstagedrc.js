288d3cbf736367abc89ae131774856b038cede7c
Ben Knáb <benknab@pm.me>
2021-05-28 14:33:27 +0200

    fix: remove type check from lint-staged
    Type check runs on the whole project, not just staged files therefore the pre-commit hook will fail
    if there are erroneous unstaged files.

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,4 @@
 module.exports = {
-	"./packages/api/**/*.{ts,tsx}": () =>
-		"tsc --project ./packages/api/tsconfig.json --noEmit",
-	"./packages/web/**/*.{ts,tsx}": () =>
-		"tsc --project ./packages/web/tsconfig.json --noEmit",
-
-	"*.{ts,tsx,js}": "eslint --cache --fix",
-
-	"*.{ts,tsx,js,jsx,json,css,md,yml,yaml}": "prettier --write",
+	"*.{ts,tsx,js,jsx}": ["eslint --cache --fix", "prettier --write"],
+	"*.{json,css,md,yml,yaml}": "prettier --write",
 }
