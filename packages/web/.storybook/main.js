0e6ed444a69eccebaacff3150509b59f6a7ca8bd
Ben Knáb <benknab@pm.me>
2021-05-28 10:37:20 +0200

    fix(web): remove background choices

--- packages/web/.storybook/main.js
+++ packages/web/.storybook/main.js
@@ -1,4 +1,12 @@
 module.exports = {
 	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-	addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+	addons: [
+		"@storybook/addon-links",
+		{
+			name: "@storybook/addon-essentials",
+			options: {
+				backgrounds: false,
+			},
+		},
+	],
 }
