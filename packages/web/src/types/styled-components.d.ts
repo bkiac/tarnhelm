3ed96235b89db852fa9eb6e92d67022c105f57ed
Ben Knáb <benknab@pm.me>
2021-06-24 15:14:06 +0200

    refactor(web): refactor styled-components theme type

--- packages/web/src/types/styled-components.d.ts
+++ packages/web/src/types/styled-components.d.ts
@@ -2,15 +2,9 @@ import "styled-components"
 import type {theme} from "../styles/theme"
 
 declare module "styled-components" {
-	export type DefaultThemeIconContentCodes = typeof theme.iconContentCodes
-	export type DefaultThemeColors = typeof theme.colors
-	export type DefaultThemePalette = typeof theme.palette
+	type Theme = typeof theme
 
 	// Must be an interface to extend `styled-components` theme
-	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-	export interface DefaultTheme {
-		iconContentCodes: DefaultThemeIconContentCodes
-		colors: DefaultThemeColors
-		palette: DefaultThemePalette
-	}
+	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions, @typescript-eslint/no-empty-interface
+	export interface DefaultTheme extends Theme {}
 }
