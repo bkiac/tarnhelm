9fcbf2eb2c8f5dca6b388869b8899dc96dd9a615
Ben Knáb <benknab@pm.me>
2021-06-26 10:34:22 +0200

    fix(web): fix emotion theme collision in storybook
    Related issues:
    - https://github.com/storybookjs/storybook/issues/12262
    - https://github.com/storybookjs/storybook/issues/10231
    
    Solution by https://github.com/storybookjs/storybook/issues/10231#issuecomment-728038867

--- packages/web/.storybook/main.js
+++ packages/web/.storybook/main.js
@@ -1,3 +1,7 @@
+const path = require("path")
+
+const toPath = (p) => path.join(process.cwd(), p)
+
 module.exports = {
 	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 	addons: [
@@ -9,4 +13,23 @@ module.exports = {
 			},
 		},
 	],
+	/**
+	 * This webpack config is needed to force Storybook to use Emotion 11:
+	 * - https://github.com/storybookjs/storybook/issues/12262
+	 * - https://github.com/storybookjs/storybook/issues/10231
+	 * - https://github.com/storybookjs/storybook/issues/10231#issuecomment-728038867
+	 */
+	webpackFinal: async (config) => {
+		return {
+			...config,
+			resolve: {
+				...config.resolve,
+				alias: {
+					...config.resolve.alias,
+					"@emotion/core": toPath("node_modules/@emotion/react"),
+					"emotion-theming": toPath("node_modules/@emotion/react"),
+				},
+			},
+		}
+	},
 }
