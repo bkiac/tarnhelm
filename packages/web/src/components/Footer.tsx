1b18960c6b2ea5b8527802b6d383dd5e490cbd68
Ben Knáb <benknab@pm.me>
2021-07-23 11:55:21 +0200

    feat(web): add responsive footer

--- packages/web/src/components/Footer.tsx
+++ packages/web/src/components/Footer.tsx
@@ -1,50 +1,39 @@
 import React from "react"
-import styled from "@emotion/styled"
+import {Box, Center, Flex, useBreakpointValue} from "@chakra-ui/react"
 import {AppTitle} from "./AppTitle"
 import {ExternalLink} from "./ExternalLink"
 import {InternalLink} from "./InternalLink"
 
-const Container = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-
-	@media screen and (max-width: 768px) {
-		flex-direction: column-reverse;
-		justify-content: space-evenly;
-		align-content: center;
-	}
-`
-
-const Item = styled.div`
-	@media screen and (max-width: 768px) {
-		margin: 0.5rem 0;
-	}
-`
-
-export const Footer: React.FunctionComponent = () => (
-	<Container>
-		<Item>
-			<AppTitle />
-		</Item>
-
-		<Item>
-			<InternalLink href="/thanks">Thanks</InternalLink>
-		</Item>
-
-		<Item>
-			<ExternalLink href="https://github.com/bkiac/tarnhelm" target="_blank">
-				Code
-			</ExternalLink>
-		</Item>
-
-		<Item>
-			<ExternalLink
-				href="https://1ml.com/node/0254aece594745b70a0ee6729c649eef57c0b5020be8cab2f4d46ff175d9333200"
-				target="_blank"
+export const Footer: React.FunctionComponent = () => {
+	const showAppTitleRow = useBreakpointValue({base: true, lg: false}) ?? true
+	const showAppTitleItem = !showAppTitleRow
+	return (
+		<Box>
+			<Flex
+				justify={{base: "space-around", lg: "space-between"}}
+				align="center"
 			>
-				Node
-			</ExternalLink>
-		</Item>
-	</Container>
-)
+				{showAppTitleItem && <AppTitle />}
+
+				<InternalLink href="/thanks">Thanks</InternalLink>
+
+				<ExternalLink href="https://github.com/bkiac/tarnhelm" target="_blank">
+					Code
+				</ExternalLink>
+
+				<ExternalLink
+					href="https://1ml.com/node/0254aece594745b70a0ee6729c649eef57c0b5020be8cab2f4d46ff175d9333200"
+					target="_blank"
+				>
+					Node
+				</ExternalLink>
+			</Flex>
+
+			{showAppTitleRow && (
+				<Center>
+					<AppTitle />
+				</Center>
+			)}
+		</Box>
+	)
+}
