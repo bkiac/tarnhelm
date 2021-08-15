cefbd7535e66e7aed4b81fe79e7ee69fffd61edf
Ben Knáb <benknab@pm.me>
2021-08-15 16:53:58 +0200

    fix(api): fix types

--- packages/api/src/lib/lnd.ts
+++ packages/api/src/lib/lnd.ts
@@ -1,16 +1,18 @@
 import * as lightning from "lightning"
 import {config} from "../config"
 
+type OmitLnd<T> = Omit<T, "lnd">
+
 export const {lnd} = lightning.authenticatedLndGrpc(config.get("lnd"))
 
 export async function createInvoice(
-	args: lightning.CreateInvoiceArgs,
+	args: OmitLnd<lightning.CreateInvoiceArgs>,
 ): Promise<lightning.CreateInvoiceResult> {
 	return lightning.createInvoice({...args, lnd})
 }
 
 export function subscribeToInvoice(
-	args: lightning.SubscribeToInvoiceArgs,
+	args: OmitLnd<lightning.SubscribeToInvoiceArgs>,
 ): ReturnType<typeof lightning.subscribeToInvoice> {
 	return lightning.subscribeToInvoice({...args, lnd})
 }
