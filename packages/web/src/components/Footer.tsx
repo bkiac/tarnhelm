b59509461676f5ff0e4db36c6c7e3aaf5992787d
Ben Knáb <benknab@pm.me>
2021-09-23 13:58:29 +0200

    feat(web): update footer styles

--- packages/web/src/components/Footer.tsx
+++ packages/web/src/components/Footer.tsx
@@ -1,37 +1,39 @@
 import React from "react"
-import {Box, Center, Flex, useBreakpointValue} from "@chakra-ui/react"
+import {Box, Center, Flex, HStack, useBreakpointValue} from "@chakra-ui/react"
 import type {BoxProps} from "@chakra-ui/react"
 import {AppTitle} from "./AppTitle"
 import {ExternalLink} from "./ExternalLink"
 import {InternalLink} from "./InternalLink"
 
 export const Footer: React.VFC<BoxProps> = (boxProps) => {
-	const showAppTitleRow = useBreakpointValue({base: true, lg: false}) ?? true
+	const showAppTitleRow = useBreakpointValue({base: true, md: false}) ?? true
 	const showAppTitleItem = !showAppTitleRow
 	return (
 		<Box as="footer" {...boxProps}>
-			<Flex
-				justify={{base: "space-around", lg: "space-between"}}
-				align="center"
-			>
+			<Flex align="center" justify={{base: "initial", md: "space-between"}}>
 				{showAppTitleItem && <AppTitle />}
-
-				<InternalLink href="/thanks">Thanks</InternalLink>
-
-				<ExternalLink href="https://github.com/bkiac/tarnhelm" target="_blank">
-					Code
-				</ExternalLink>
-
-				<ExternalLink
-					href="https://1ml.com/node/0254aece594745b70a0ee6729c649eef57c0b5020be8cab2f4d46ff175d9333200"
-					target="_blank"
+				<HStack
+					w={{base: "100%", md: "initial"}}
+					spacing={{base: "initial", md: "10"}}
+					justifyContent={{base: "space-between", sm: "space-around"}}
 				>
-					Node
-				</ExternalLink>
+					<InternalLink href="/thanks">Thanks</InternalLink>
+					<ExternalLink
+						href="https://github.com/bkiac/tarnhelm"
+						target="_blank"
+					>
+						Code
+					</ExternalLink>
+					<ExternalLink
+						href="https://1ml.com/node/0254aece594745b70a0ee6729c649eef57c0b5020be8cab2f4d46ff175d9333200"
+						target="_blank"
+					>
+						Node
+					</ExternalLink>
+				</HStack>
 			</Flex>
-
 			{showAppTitleRow && (
-				<Center>
+				<Center mt="2">
 					<AppTitle />
 				</Center>
 			)}
