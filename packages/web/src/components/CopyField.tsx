fdcd749c8eba219ea2e9e637ceaccae0abc36d6d
Ben Knáb <benknab@pm.me>
2021-09-28 20:20:39 +0200

    feat(web): add click handler and noisy SVGs

--- packages/web/src/components/CopyField.tsx
+++ packages/web/src/components/CopyField.tsx
@@ -1,17 +1,52 @@
-import React from "react"
+import React, {useCallback} from "react"
 import styled from "@emotion/styled"
 import {css} from "@emotion/react"
 import {Box, Flex} from "@chakra-ui/react"
-import {Button} from "./Button"
-import {DangerIcon} from "./DangerIcon"
+import {CopyIcon} from "@chakra-ui/icons"
 import {IconButton} from "./IconButton"
-import {noise} from "../styles/animations"
+import {noise, noiseSnippet} from "../styles/animations"
 
 const noiseArgs = {
 	steps: 66,
 	fraction: 2,
 	duration: 3,
 }
+
+const Icon = styled(CopyIcon)`
+	vertical-align: initial;
+`
+
+const IconBeforeAfter = styled(Icon)`
+	position: absolute;
+	top: 0;
+	left: 0;
+	overflow: hidden;
+
+	&:hover {
+		${noise(noiseArgs)};
+	}
+`
+
+const IconBefore = styled(IconBeforeAfter)((props) => {
+	const [k, p] = noiseSnippet(noiseArgs)
+	return css`
+		&:hover {
+			filter: drop-shadow(0.05em 0.025em 0 ${props.theme.colors.primary});
+			animation: ${k} ${p};
+		}
+	`
+})
+
+const IconAfter = styled(IconBeforeAfter)((props) => {
+	const [k, p] = noiseSnippet(noiseArgs)
+	return css`
+		&:hover {
+			left: 1px;
+			filter: drop-shadow(0.025em 0.05em 0 ${props.theme.colors.secondary});
+			animation: ${k} ${p};
+		}
+	`
+})
 
 const StyledIconButton = styled(IconButton)((props) => {
 	const fontSize = props.theme.fontSizes["2xl"]
@@ -27,28 +62,6 @@ const StyledIconButton = styled(IconButton)((props) => {
 
 		/** Animation */
 		position: relative;
-		&:before,
-		&:after {
-			content: "${props.theme.iconContentCodes.danger}";
-			position: absolute;
-			top: 0;
-			left: 0;
-			overflow: hidden;
-			background: ${props.theme.colors.background};
-		}
-
-		&:hover {
-			&:before {
-				text-shadow: 0.05em 0.025em ${props.theme.colors.secondary};
-			}
-
-			&:after {
-				left: 1px;
-				text-shadow: 0.025em 0.05em ${props.theme.colors.primary};
-			}
-
-			${noise(noiseArgs)};
-		}
 	`
 })
 
@@ -58,54 +71,35 @@ const B = styled(Box)(
 		border-style: solid;
 		border-color: white;
 		padding: ${props.theme.space["1"]};
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
 	`,
 )
 
 const B2 = styled(Box)(
 	(props) => css`
-		border: 1px solid white;
+		border-width: 1px 1px 1px 0;
+		border-style: solid;
+		border-color: white;
 		padding: ${props.theme.space["1"]};
 	`,
 )
 
-export const CopyField: React.VFC = () => {
-	const ye
+export const CopyField: React.VFC<{value: string}> = ({value}) => {
+	const handleClick = useCallback(() => {
+		void navigator.clipboard.writeText(value)
+	}, [value])
 	return (
-		<div>
-			<Flex align="center" mb="2">
-				<B>
-					lnbcrt500u1ps5ettwpp5y5zt2hr5s5gezwj7cqh59d4www4jv6phuqy22r0rde4usvq4zrzsdqqcqzpgxqyz5vqsp57ghfpfuk6pguuwwnuwes0vt37as26740mejad0u3urvrwddzcm5q9qyyssq7mekfcl0k2vzhsqmuf4c5z7mhm702ka3cagq5yj2khnk29w5lku3nknd0fg75aemydvyxhl3sm8va3x7x97g02d0skr20ej9tsyypdsqdmg4uu
-				</B>
-				<B2 color="broom">copy</B2>
-				<p>too bland</p>
-			</Flex>
-			<Flex align="center" mb="2">
-				<B>
-					lnbcrt500u1ps5ettwpp5y5zt2hr5s5gezwj7cqh59d4www4jv6phuqy22r0rde4usvq4zrzsdqqcqzpgxqyz5vqsp57ghfpfuk6pguuwwnuwes0vt37as26740mejad0u3urvrwddzcm5q9qyyssq7mekfcl0k2vzhsqmuf4c5z7mhm702ka3cagq5yj2khnk29w5lku3nknd0fg75aemydvyxhl3sm8va3x7x97g02d0skr20ej9tsyypdsqdmg4uu
-				</B>
-				<Button>copy</Button>
-				<p>i dont want to reuse the main button style</p>
-			</Flex>
-			<Flex align="center" mb="2">
-				<B>
-					lnbcrt500u1ps5ettwpp5y5zt2hr5s5gezwj7cqh59d4www4jv6phuqy22r0rde4usvq4zrzsdqqcqzpgxqyz5vqsp57ghfpfuk6pguuwwnuwes0vt37as26740mejad0u3urvrwddzcm5q9qyyssq7mekfcl0k2vzhsqmuf4c5z7mhm702ka3cagq5yj2khnk29w5lku3nknd0fg75aemydvyxhl3sm8va3x7x97g02d0skr20ej9tsyypdsqdmg4uu
-				</B>
-				<B2>
-					<IconButton>
-						<DangerIcon />
-					</IconButton>
-				</B2>
-			</Flex>
-			<Flex align="center" mb="2">
-				<B>
-					lnbcrt500u1ps5ettwpp5y5zt2hr5s5gezwj7cqh59d4www4jv6phuqy22r0rde4usvq4zrzsdqqcqzpgxqyz5vqsp57ghfpfuk6pguuwwnuwes0vt37as26740mejad0u3urvrwddzcm5q9qyyssq7mekfcl0k2vzhsqmuf4c5z7mhm702ka3cagq5yj2khnk29w5lku3nknd0fg75aemydvyxhl3sm8va3x7x97g02d0skr20ej9tsyypdsqdmg4uu
-				</B>
-				<B2>
-					<StyledIconButton>
-						<DangerIcon />
-					</StyledIconButton>
-				</B2>
-			</Flex>
-		</div>
+		<Flex align="center" mb="2">
+			<B>{value}</B>
+			<B2 bg="tertiary">
+				<StyledIconButton onClick={handleClick}>
+					<IconBefore />
+					<Icon />
+					<IconAfter />
+				</StyledIconButton>
+			</B2>
+		</Flex>
 	)
 }
