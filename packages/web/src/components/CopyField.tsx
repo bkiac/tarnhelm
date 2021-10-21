029505003469f2d222c02b35aa55e62a903b440a
Ben Knáb <benknab@pm.me>
2021-10-21 19:59:26 +0200

    feat(web): add flex props to copy field

--- packages/web/src/components/CopyField.tsx
+++ packages/web/src/components/CopyField.tsx
@@ -1,14 +1,18 @@
 import React, {useCallback} from "react"
 import {Box, Flex} from "@chakra-ui/react"
 import {CopyIcon} from "@chakra-ui/icons"
+import type {FlexProps} from "@chakra-ui/react"
 import {NoisyIconButton} from "./NoisyIconButton"
 
-export const CopyField: React.VFC<{value: string}> = ({value}) => {
+export const CopyField: React.VFC<{value: string} & FlexProps> = ({
+	value,
+	...flexProps
+}) => {
 	const handleClick = useCallback(() => {
 		void navigator.clipboard.writeText(value)
 	}, [value])
 	return (
-		<Flex align="center" border="1px" borderColor="white">
+		<Flex align="center" border="1px" borderColor="white" {...flexProps}>
 			<Box overflow="hidden" whiteSpace="nowrap" textOverflow="ellipsis" px="1">
 				{value}
 			</Box>
