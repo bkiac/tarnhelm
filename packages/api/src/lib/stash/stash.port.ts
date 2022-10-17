6aa54756ce5461a762874f8b5245a8dc72bdc67e
Ben Knáb <benknab@pm.me>
2022-10-17 20:17:48 +0200

    Update types

--- packages/api/src/lib/stash/stash.port.ts
+++ packages/api/src/lib/stash/stash.port.ts
@@ -11,20 +11,43 @@ export namespace Stash {
 
 	export type Listener = (progress: Progress) => void
 
-	export type Data = {
-		key: Key
-		location: string
+	export type Blob = {
+		readonly key: Key
+		readonly location: string
+		/** base64 string */
+		readonly auth: string
+		/** Additional properties (file name, type, etc.) encrypted before upload */
+		readonly properties: string
+		readonly length: number
+		nonce: string
+		downloads: {
+			current: number
+			readonly limit: number
+		}
+	}
+
+	export type BlobUploadInput = {
+		limit: number
+		body: stream.Readable
+		length: number
+		expiry: Date
+	} & Pick<Blob, "key" | "auth" | "properties" | "nonce">
+
+	export type BlobUpdateInput = {
+		numOfDownloads?: number
+		nonce?: string
 	}
 
 	export type Client = {
-		set: (
-			key: Key,
-			body: stream.Readable,
-			length: number,
+		upload: (
+			input: BlobUploadInput,
 			listener?: Listener,
-		) => r.PromiseResult<Data>
-		get: (key: Key) => r.Result<stream.Readable>
-		delete: (...keys: Key[]) => r.PromiseResult<void>
-		list: (limit: number, cursor: string) => r.Result<Key[]>
+		) => r.PromiseResult<Blob>
+		download: (key: Key) => r.Result<stream.Readable>
+		get: (key: Key) => r.PromiseResult<Blob>
+		update: (input: BlobUpdateInput) => r.PromiseResult<Blob>
+		delete: (key: Key) => r.PromiseResult<Key>
+		bumpDownloads: (key: Key) => r.PromiseResult<Blob>
+		updateNonce: (key: Key, nonce: string) => r.PromiseResult<Blob>
 	}
 }
