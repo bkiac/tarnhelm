d412c13d2cb935a59d1e046badcaa5c584e85ec4
bkiac <bkiac@pm.me>
2023-08-02 08:36:58 +0200

    Add proto copy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	webpack: (config, { isServer }) => {
+		// Only run in server mode
+		if (isServer) {
+		  config.plugins.push(
+			new CopyPlugin({
+			  patterns: [
+				{ 
+				  from: 'node_modules/lightning/grpc/protos', 
+				  to: './grpc/protos' 
+				},
+			  ],
+			})
+		  );
+		}
+		return config;
+	},
+}
 
 module.exports = nextConfig
